bb9df1405d29d7f4eec82b940cdc11c335d5c869
Daniel Dittenhafer <daniel@dittenhafer.net>
2016-12-06 22:06:07 -0500

    minor proposal update

--- src/main.py
+++ src/main.py
@@ -10,15 +10,16 @@ from linkedin import linkedin
 _CLIENTID = "78mswaaquqtli4"
 _CLIENTSECRET = "aU4kKE5rRSkuwvZj"
 
-_AUTHORIZATION_CODE = "AQR83lzo5lZJgjldp7wlRe1ZEEaJtEvYxrr9V25ecTLe0WWwmMzdHeIN_WcWd-n7cZw9xL5b7ckwH6RaKrzFsDKO3IpAM6Jb_X-yqwI1RCPrGLwa7rw"
+_AUTHORIZATION_CODE = "AQQLynVv-_kRyA_4iQTeTPPIjv44mu9tGwZycumWICpVyUgbf9fRtwAKG7WI-6Dayi_nBz8V_statGcfuyGjYuAoJR5m-XFl41rPhxzXlqz6iekQHCo" \
+                      #"AQR83lzo5lZJgjldp7wlRe1ZEEaJtEvYxrr9V25ecTLe0WWwmMzdHeIN_WcWd-n7cZw9xL5b7ckwH6RaKrzFsDKO3IpAM6Jb_X-yqwI1RCPrGLwa7rw"
 
 _ACCESS_TOKEN = "AQX7pBIXjAccD6APfjBWzElRkmMRxJk8qkaQ0HZL5t8glJN4pN2tZ9Ja8pXGMUVFVnkpbXXZ5s7SHMZmTKtiYWGgVpIWD_Xhsn0m_D1pliukshg1w6e0NpkMp1tbWK57LdAHxaXKafUSGa_O5Ss1rqLlbj_kRuQhU0ggt_k95iZ0eZUbXtQ"
 _ACCESS_TOKEN_EXPIRES_IN = 5183999
 
-def main():
+def linkedin_test():
     authentication = linkedin.LinkedInAuthentication(_CLIENTID, _CLIENTSECRET, "http://localhost/",
                                                      [linkedin.PERMISSIONS.BASIC_PROFILE])
-    #authentication = linkedin.LinkedInDeveloperAuthentication(_CLIENTID, _CLIENTSECRET,
+    # authentication = linkedin.LinkedInDeveloperAuthentication(_CLIENTID, _CLIENTSECRET,
     #                                                          USER_TOKEN, USER_SECRET,
     #                                                          RETURN_URL, linkedin.PERMISSIONS.enums.values())
 
@@ -26,9 +27,8 @@ def main():
     print authentication.authorization_url
     authentication.token = linkedin.AccessToken(_ACCESS_TOKEN, _ACCESS_TOKEN_EXPIRES_IN)
 
-
-    #authentication.authorization_code = _AUTHORIZATION_CODE
-    #authentication.get_access_token()
+    # authentication.authorization_code = _AUTHORIZATION_CODE
+    # authentication.get_access_token()
     print authentication.token
 
     # Pass it in to the app...
@@ -40,11 +40,13 @@ def main():
     g = application.get_profile(selectors="first-name,last-name,formatted-name,positions")
     print g
 
-    params = {}
-    params["first-name"] = "Justin"
-    params["last-name"] = "Hink"
-    g = application.search_profile("people:(id,first-name,last-name)", params)
+    params = {'keywords': 'Justin Hink'}
+
+    g = application.search_profile([{'people': ['first-name', 'last-name']}], params)
     print g
+
+def main():
+    linkedin_test()
 
 
 # This is the main of the program.
