5247980703a400dbd8ce8f88bc824172342a3153
Daniel Dittenhafer <daniel@dittenhafer.net>
2016-12-13 21:55:05 -0500

    twitter api + some followers (incomplete)

--- src/main.py
+++ src/main.py
@@ -6,6 +6,10 @@
 # Description: Main entry point for Trump Cabinet network analysis.
 #
 from linkedin import linkedin
+import twitter
+import csv
+import os.path
+import time
 
 _CLIENTID = "78mswaaquqtli4"
 _CLIENTSECRET = "aU4kKE5rRSkuwvZj"
@@ -45,8 +49,53 @@ def linkedin_test():
     g = application.search_profile([{'people': ['first-name', 'last-name']}], params)
     print g
 
+def twitter_test(screenname):
+
+    api = twitter.Api(consumer_key='UzF8FKAMeVc1crLEhUtTQyEgI',
+                           consumer_secret='uKdaftSyh9opBfeJ5ohVlK1HkA7xGB5hMDNpMO7dX8nOmeto5H',
+                           access_token_key='85418124-FzzYpLl4BPwNjPTNqWWQtFm2D9xEttazXfL6Px61O',
+                           access_token_secret='K4LudWpqqz86b6A1DcbpdSw1vYVk6XDBcNJkPqDL816cn')
+
+    print(api.VerifyCredentials())
+
+    bContinue = True
+    cursor = -1
+    followDict = {}
+    file = 'trump_followers_plus.csv'
+    mode = 'w'
+
+    if os.path.isfile(file):
+        mode = 'a'
+        with open(file, 'r') as mycsvfile:
+            dw = csv.reader(mycsvfile)
+            for f in dw:
+                followDict[f[0]] = int(f[1])
+
+    while bContinue:
+
+        next_cursor, previous_cursor, followers = api.GetFollowersPaged(screen_name=screenname, count=200, skip_status=True)
+
+        print(followers[1])
+        with open(file, mode) as mycsvfile:
+            dw = csv.writer(mycsvfile)
+            for f in followers:
+                if(f.screen_name not in followDict):
+                    followDict[f.screen_name] = f.followers_count
+                    dw.writerow([f.screen_name, f.followers_count, f.lang, f.location, f.time_zone])
+
+        if next_cursor:
+            cursor = next_cursor
+            time.sleep(60)
+
+        if next_cursor == 0 or next_cursor == previous_cursor:
+            break
+
+    return None
+
 def main():
-    linkedin_test()
+    #linkedin_test()
+
+    twitter_test("realDonaldTrump")
 
 
 # This is the main of the program.
